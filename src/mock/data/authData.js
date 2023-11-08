3f67d87d4706d2a49b034d29c891ba53e70639b8
Syed Ruma Ali <RumaAli266>
2023-11-08 21:36:15 +0500

    elstar dashboard

---
+++ src/mock/data/authData.js
@@ -0,0 +1,11 @@
+export const signInUserData = [
+    {
+        id: '21',
+        avatar: '/img/avatars/thumb-10.jpg',
+        userName: 'Syed Ruma Ali',
+        email: 'syedrumaali266@gmail.com',
+        authority: ['admin', 'user'],
+        password: '123Qwe',
+        accountUserName: 'admin',
+    },
+]
